e2d4400053e84834f70cc24e23412e59e3156963
meikizedek1327 <meikizedek1327@gmail.com>
2026-07-11 00:54:06 +0100

    Created A program that asks for one's first and last name and returns the output

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("Press Enter to exit...");
+        Console.ReadLine();
     }
 }
