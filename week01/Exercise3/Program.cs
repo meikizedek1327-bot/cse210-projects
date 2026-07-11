f44668f77ffe240898c8804b846863396c0c25ee
meikizedek1327 <meikizedek1327@gmail.com>
2026-07-11 01:23:50 +0100

    New guess game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,49 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+   static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+        Random random = new Random();
+
+        while (playAgain.ToLower() == "yes")
+        {
+            // Here I generate a random magic number between 1 and 100
+            int magicNumber = random.Next(1, 21);
+            int guess = 0;
+            int guessCount = 0;
+
+            Console.WriteLine("Welcome to the Guess My Number game!");
+
+            // It keeps asking until the user guesses correctly
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            Console.WriteLine($"It took you {guessCount} guesses.");
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
